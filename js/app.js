77f27cbcb60940d1e8d19020f4d6fc049573244a
Laura Comte <comtelaurapro@gmail.com>
2023-06-06 13:58:33 +0200

    new exo

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@ Le modal disparait lorsqu'on recommence le cycle.
 Un deuxième bouton doit servir à masquer le modal.
 
 */
+
+// left = "1270px";
+// top = "650px";
+
+const container = document.getElementById("container");
+const moveDiv = document.getElementById("moveDiv");
+const array = ["calc(100% - 200px)"]
+
+moveDiv.addEventListener("click", (event) => {
+  container.classList.add("right-top");
+
+//   if (container.classList === "right-top") {
+//     container.classList.remove("right-top");
+//   }
+
+    for (let i =0; i < ; i++) {
+      container.classList.add("[i]")
+    }
+});
